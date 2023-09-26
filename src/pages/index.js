b91e16aaa129cd20358489ad2b491e649005c8d9
Josef Pihrt <josef@pihrt.net>
2023-09-26 16:07:34 +0200

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -23,7 +23,7 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
+      title={`${siteConfig.title} - documentation for my open source projects`}
       description="Description will go into a meta tag in <head />">
       <HomepageHeader />
     </Layout>
